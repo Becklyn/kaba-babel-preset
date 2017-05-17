699ad79c3aba0291f0cb13d388f5fa9a2bf1e62c
Jannik Zschiesche <jz@becklyn.com>
2017-05-17 15:53:30 +0200

    Add first implementation of transform

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    plugins: [
+        ["babel-plugin-transform-es2015-template-literals", {loose: false, spec: false}],
+        ["babel-plugin-transform-es2015-literals"],
+        ["babel-plugin-transform-es2015-function-name"],
+        ["babel-plugin-transform-es2015-arrow-functions", {spec: false}],
+        ["babel-plugin-transform-es2015-block-scoped-functions"],
+        ["babel-plugin-transform-es2015-classes", {loose: false}],
+        ["babel-plugin-transform-es2015-object-super"],
+        ["babel-plugin-transform-es2015-shorthand-properties"],
+        ["babel-plugin-transform-es2015-duplicate-keys"],
+        ["babel-plugin-transform-es2015-computed-properties", {loose: false}],
+        ["babel-plugin-transform-es2015-for-of", {loose: false}],
+        ["babel-plugin-transform-es2015-sticky-regex"],
+        ["babel-plugin-transform-es2015-unicode-regex"],
+        ["babel-plugin-check-es2015-constants"],
+        ["babel-plugin-transform-es2015-spread", {loose: false}],
+        ["babel-plugin-transform-es2015-parameters"],
+        ["babel-plugin-transform-es2015-destructuring", {loose: false}],
+        ["babel-plugin-transform-es2015-block-scoping"],
+    ],
+}
