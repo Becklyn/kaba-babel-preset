40425f28427eed861e3b9df55d4f1539116eef12
Jannik Zschiesche <jz@becklyn.com>
2019-06-27 19:55:01 +0200

    Just use `@babel/preset-env` (#2)
    * Just use `@babel/preset-env`
    
    It simplifies everything and allows browserlist-dependent builds

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // the plugins are applied from top to bottom, so the order is important.
-    // Especially the syntax plugins (like class properties) need to be all other plugins.
     plugins: [
         // ------------------------------------------------------------------------------------------
         // Stage 3 proposals
@@ -12,30 +10,7 @@ module.exports = {
         // https://babeljs.io/docs/plugins/transform-class-properties/
         ["@babel/plugin-proposal-class-properties", {loose: true}],
 
-
-        // ------------------------------------------------------------------------------------------
-        // JSX
-        // ------------------------------------------------------------------------------------------
-        ["@babel/plugin-transform-react-jsx", {pragma: "h"}],
-
-        // ------------------------------------------------------------------------------------------
-        // ES 2015
-        // ------------------------------------------------------------------------------------------
-        ["@babel/plugin-transform-arrow-functions", {spec: false}],
-        ["@babel/plugin-transform-block-scoped-functions"],
-        ["@babel/plugin-transform-block-scoping"],
-        ["@babel/plugin-transform-classes"],
-        ["@babel/plugin-transform-computed-properties", {loose: true}],
-        ["@babel/plugin-transform-destructuring", {loose: false}],
-        ["@babel/plugin-transform-duplicate-keys"],
-        ["@babel/plugin-transform-function-name"],
-        ["@babel/plugin-transform-literals"],
-        ["@babel/plugin-transform-object-super"],
-        ["@babel/plugin-transform-parameters", {loose: true}],
-        ["@babel/plugin-transform-shorthand-properties"],
-        ["@babel/plugin-transform-spread"],
-        ["@babel/plugin-transform-sticky-regex"],
-        ["@babel/plugin-transform-template-literals", {loose: true}],
-        ["@babel/plugin-transform-unicode-regex"],
-    ],
+        // default env
+        ["@babel/preset-env", {spec: false}],
+    ]
 };
