5b188fa555e15a94571eff41affb26988183be4e
Jannik Zschiesche <jz@becklyn.com>
2019-06-27 19:54:09 +0200

    Just use `@babel/preset-env`
    It simplifies everything and allows browserlist-dependent builds

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-let modern = [
-    // ------------------------------------------------------------------------------------------
-    // Stage 3 proposals
-    // ------------------------------------------------------------------------------------------
-    ["@babel/plugin-syntax-dynamic-import"],
-    ["@babel/plugin-proposal-json-strings"],
-
-    // set with loose: true, as the compilation is pretty big
-    // https://babeljs.io/docs/plugins/transform-class-properties/
-    ["@babel/plugin-proposal-class-properties", {loose: true}],
-];
-
-
 module.exports = {
-    module: {
-        plugins: modern,
-    },
-    // all
-    legacy: {
-        // the plugins are applied from top to bottom, so the order is important.
-        // Especially the syntax plugins (like class properties) need to be all other plugins.
-        plugins: modern.concat([
-            // ------------------------------------------------------------------------------------------
-            // ES 2015
-            // ------------------------------------------------------------------------------------------
-            ["@babel/plugin-transform-arrow-functions", {spec: false}],
-            ["@babel/plugin-transform-block-scoped-functions"],
-            ["@babel/plugin-transform-block-scoping"],
-            ["@babel/plugin-transform-classes"],
-            ["@babel/plugin-transform-computed-properties", {loose: true}],
-            ["@babel/plugin-transform-destructuring", {loose: false}],
-            ["@babel/plugin-transform-duplicate-keys"],
-            ["@babel/plugin-transform-function-name"],
-            ["@babel/plugin-transform-literals"],
-            ["@babel/plugin-transform-object-super"],
-            ["@babel/plugin-transform-parameters", {loose: true}],
-            ["@babel/plugin-transform-shorthand-properties"],
-            ["@babel/plugin-transform-spread"],
-            ["@babel/plugin-transform-sticky-regex"],
-            ["@babel/plugin-transform-template-literals", {loose: true}],
-            ["@babel/plugin-transform-unicode-regex"],
-        ]),
-    },
+    plugins: [
+        // ------------------------------------------------------------------------------------------
+        // Stage 3 proposals
+        // ------------------------------------------------------------------------------------------
+        ["@babel/plugin-syntax-dynamic-import"],
+        ["@babel/plugin-proposal-json-strings"],
+
+        // set with loose: true, as the compilation is pretty big
+        // https://babeljs.io/docs/plugins/transform-class-properties/
+        ["@babel/plugin-proposal-class-properties", {loose: true}],
+
+        // default env
+        ["@babel/preset-env", {spec: false}],
+    ]
 };
